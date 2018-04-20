513b4402097f622b18cad319e034b2570c27e102
Yan Virin <yvirin@apixio.com>
2018-04-20 11:31:29 -0700

    smaller train set for evaluation

--- jvm-packages/xgboost4j-spark/src/main/scala/ml/dmlc/xgboost4j/scala/spark/XGBoost.scala
+++ jvm-packages/xgboost4j-spark/src/main/scala/ml/dmlc/xgboost4j/scala/spark/XGBoost.scala
@@ -477,12 +477,16 @@ private class Watches private(
   val test: DMatrix,
   private val cacheDirName: Option[String]) {
 
-  def toMap: Map[String, DMatrix] = Map("train" -> train, "test" -> test)
+  // special batch for training error
+  val batch = train.slice((0 until (train.rowNum / 10).toInt).toArray)
+
+  def toMap: Map[String, DMatrix] = Map("batch" -> batch, "test" -> test)
     .filter { case (_, matrix) => matrix.rowNum > 0 }
 
   def size: Int = toMap.size
 
   def delete(): Unit = {
+    train.delete()
     toMap.values.foreach(_.delete())
     cacheDirName.foreach { name =>
       FileUtils.deleteDirectory(new File(name))
